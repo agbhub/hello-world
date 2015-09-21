57871e02c84c16c77ab8fc38e8f0c90097ac331a
Amita Barochia <agb09@hotmail.com>
2015-09-21 19:08:04 -0400

    Added the date to the program

--- hello-world/src/main.java
+++ hello-world/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.print("Hello World!");
+         System.out.print("Hello World! Monday 9/21/2015");
 			}
 
 }
